6d792d2c46e39eda5e235f88e883065ccd23d2fb
Vitalii Ostrovskyi <vitos@vitos.org.ua>
2010-10-13 02:11:16 +0000

    Do simple vortex pair rotation.

--- src/main.c
+++ src/main.c
@@ -6,20 +6,41 @@
 int main() {
 	hvs_state* state;
 	hvs_params params;
-	int status;
+	int status,i;
 	params.initvortfile = "test";
-	params.gridwidth = 100;
-	params.gridheight = 100;
-	params.timestep = 0.1;
-	if ((status = init_solver(&params, &state)) != HVS_OK) {
+	params.timestep = 0.001;
+	params.lambda0 = 1.0;
+	params.nu = 0.1;
+	hvs_center centers[] = {{-1.0,0.0},{1.0,0.0}};
+	hvs_moment moment1;
+	moment1[0] = 1.0;
+	moment1[1] = 0.0;
+	moment1[2] = 0.0;
+	moment1[3] = 0.0;
+	moment1[4] = 0.0;
+	moment1[5] = 0.0;
+	hvs_moment moment2 = {1.0,0.0,0.0,0.0,0.0,0.0};
+	hvs_moment moments[2];
+	memcpy(moments[0],moment1,sizeof(hvs_moment));
+	memcpy(moments[1],moment2,sizeof(hvs_moment));
+	if ((status = init_solver_by_moments(&params, 2, centers, moments,
+				-5.0, 5.0, 0.1, 
+				-5.0, 5.0, 0.1, &state)) != HVS_OK) {
 		hvserror(status, "Init error");
 		return 0;
 	}
-	if ((status = run_solver(&params, state)) == HVS_OK) {
-		printf("Size: %i\n", state->size);
-		write_output(state,"test.out");
-	} else {
-		hvserror(status,"Run error");
+	for (i=0;i<10;i++) {
+		params.t0 = 0.1*i;
+		params.t1 = 0.1*(i+1);
+		if ((status = run_solver(&params, state)) == HVS_OK) {
+			printf("Centers: (%.4f,%.4f),(%.4f,%.4f)\n", 
+				state->centers[0].x,state->centers[0].y,
+				state->centers[1].x,state->centers[1].y);
+			write_output(state,"test.out");
+		} else {
+			hvserror(status,"Run error");
+			break;
+		}
 	}
 	free_solver(&state);
 	return 0;
