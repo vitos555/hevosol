558e30ee1751975d248597e8fbcbf61a041b0a88
Vitalii Ostrovskyi <vitos@vitos.org.ua>
2010-11-12 10:45:57 +0000

    Fixed arnoldi iterations and ols.

--- src/libhvs/gmres.c
+++ src/libhvs/gmres.c
@@ -6,7 +6,7 @@
 
 int gmres(const FLOAT_TYPE *A, const FLOAT_TYPE *x0, const FLOAT_TYPE *b, UINT size,
 		FLOAT_TYPE accuracy, UINT restart, FLOAT_TYPE *result) {
-	FLOAT_TYPE beta;
+	FLOAT_TYPE beta,bnormval;
 	FLOAT_TYPE residualsq;
 	FLOAT_TYPE *betae = NULL,*yn = NULL,*bnorm = NULL, *tmp = NULL;
 	FLOAT_TYPE *H = NULL,*Q = NULL;
@@ -24,7 +24,8 @@ int gmres(const FLOAT_TYPE *A, const FLOAT_TYPE *x0, const FLOAT_TYPE *b, UINT s
 	}
 
 	// Normalize b
-	if ((status=vect_scalarmult(1/M_SQRT(vect_normsq(b,size)),b,size,bnorm))!=HVS_OK) {
+	bnormval = M_SQRT(vect_normsq(b,size));
+	if ((status=vect_scalarmult((FLOAT_TYPE)1/bnormval,b,size,bnorm))!=HVS_OK) {
 		return status;
 	}
 	
@@ -39,7 +40,6 @@ int gmres(const FLOAT_TYPE *A, const FLOAT_TYPE *x0, const FLOAT_TYPE *b, UINT s
 		}
 		beta = M_SQRT(vect_normsq(tmp,size));
 		n = 0;
-		
 		while((residualsq>(accuracy*accuracy)) && (n<restart)) {
 			n++;
 			if (globaln==1) {
@@ -53,14 +53,14 @@ int gmres(const FLOAT_TYPE *A, const FLOAT_TYPE *x0, const FLOAT_TYPE *b, UINT s
 					free(tmp);
 					return HVS_ERR;
 				}
-				if ((H = realloc(H,sizeof(FLOAT_TYPE)*(n+1)*n))==NULL) {
+				if ((H = realloc(H,sizeof(FLOAT_TYPE)*n*(n+1)))==NULL) {
 					free(betae);
 					free(bnorm);
 					free(tmp);
 					free(yn);
 					return HVS_ERR;
 				}
-				if ((Q = realloc(Q,sizeof(FLOAT_TYPE)*size*n))==NULL) {
+				if ((Q = realloc(Q,sizeof(FLOAT_TYPE)*size*(n+1)))==NULL) {
 					free(betae);
 					free(bnorm);
 					free(tmp);
@@ -69,22 +69,33 @@ int gmres(const FLOAT_TYPE *A, const FLOAT_TYPE *x0, const FLOAT_TYPE *b, UINT s
 					return HVS_ERR;
 				}
 			}
-			memset(H,0,sizeof(FLOAT_TYPE)*(n+1)*n);
+			if (n>1) {
+				memset(&H[n*(n-1)],0,sizeof(FLOAT_TYPE)*n*2);
+				betae[n-1]=0;
+				for(i=1;i<=n;i++) {
+					memcpy(betae,&H[(n-i)*(n-1)],sizeof(FLOAT_TYPE)*(n-1));
+					memcpy(&H[(n-i)*n],betae,sizeof(FLOAT_TYPE)*n);
+				}
+			}
+			if (n==1) {
+				memcpy(Q,bnorm,sizeof(FLOAT_TYPE)*size);
+			}
 			memset(betae,0,sizeof(FLOAT_TYPE)*(n+1));
 			betae[0]=beta;
 
 			// One step Arnoldi
-			if ((status = arnoldi(A,b,size,n,H,Q))!=HVS_OK) {
+			if ((status = arnoldi(A,size,n,H,Q))!=HVS_OK) {
 				return status;
 			}
 			// Now OLS
 			if ((status = ols(H,betae,n+1,n,yn))!=HVS_OK) {
 				return status;
 			}
-		
+			
 			// Get rn into result
 			for(i=0;i<n+1;i++) {
-				result[i] = vect_dotproduct(&H[i*n],yn,n)-betae[i];
+				result[i] = vect_dotproduct(&H[i*n],yn,n);
+				if (i==0) result[i] -= beta;
 			}
 			residualsq = vect_normsq(result,n+1);
 			printf("Residual: %Lf\n",residualsq);
@@ -92,7 +103,11 @@ int gmres(const FLOAT_TYPE *A, const FLOAT_TYPE *x0, const FLOAT_TYPE *b, UINT s
 	
 		// Get xn
 		for(i=0;i<size;i++) {
-			result[i]=M_SQRT(vect_normsq(b,size))*vect_dotproduct(&Q[i*n],yn,n);
+			int j;
+			result[i] = (FLOAT_TYPE)0.0;
+			for (j=0;j<n;j++) {
+				result[i]+=Q[j*size+i]*yn[j];
+			}
 		}
 	}
 	free(H);
@@ -110,13 +125,6 @@ int ols(const FLOAT_TYPE *X, const FLOAT_TYPE *y, UINT m, UINT n, FLOAT_TYPE *re
 	UINT i,j,k;
 	int status;
 	
-	for(i=0;i<n;i++) {
-		for(j=0;j<m;j++)
-			printf("%Lf\t",X[i*n+j]);
-		printf("\n");
-	}
-	printf("\n");
-
 	if ((inv=malloc(sizeof(FLOAT_TYPE)*n*n))==NULL) {
 		return HVS_ERR;
 	}
@@ -130,22 +138,16 @@ int ols(const FLOAT_TYPE *X, const FLOAT_TYPE *y, UINT m, UINT n, FLOAT_TYPE *re
 		for(j=0;j<n;j++) {
 			xx[i*n+j]=0;
 			for (k=0;k<m;k++) 
-				xx[i*n+j] = X[i*n+k]*X[j*n+k];
+				xx[i*n+j] += X[k*n+i]*X[k*n+j];
 		}
 		result[i] = 0;
 	}
 	
-	for(i=0;i<n;i++) {
-		for(j=0;j<n;j++)
-			printf("%Lf\t",xx[i*n+j]);
-		printf("\n");
-	}
 	// Find inverse
 	if((status=matrix_inv(xx,n,inv))!=HVS_OK) {
 		return status;
 	}
 	
-			printf("Here\n");
 	for(i=0;i<n;i++)
 		for(j=0;j<m;j++)
 			for (k=0;k<n;k++)
@@ -155,37 +157,37 @@ int ols(const FLOAT_TYPE *X, const FLOAT_TYPE *y, UINT m, UINT n, FLOAT_TYPE *re
 	return HVS_OK;
 }
 
-int arnoldi(const FLOAT_TYPE *A, const FLOAT_TYPE *q1, UINT m, UINT n, FLOAT_TYPE *H,
+int arnoldi(const FLOAT_TYPE *A, UINT m, UINT n, FLOAT_TYPE *H,
 		FLOAT_TYPE *Q) {
 	UINT k,j,i;
-	FLOAT_TYPE *qk,*qkp;
 	int status;
-	if ((qk = malloc(sizeof(FLOAT_TYPE)*m)) ==NULL) {
-		return HVS_ERR;
+
+	if ((status=vect_matrixmult(A,&Q[(n-1)*m],m,&Q[n*m]))!=HVS_OK) {
+		return status;
 	}
-	if ((qkp = malloc(sizeof(FLOAT_TYPE)*m)) ==NULL) {
-		free(qk);
-		return HVS_ERR;
-	}
-	memcpy(qkp,q1,sizeof(FLOAT_TYPE)*m);
-	for (k=1;k<n+1;k++) {
-		for(i=0;i<m;i++) Q[i*n+k-1]=qkp[i];
-		if ((status=vect_matrixmult(A,qkp,m,qk))!=HVS_OK) {
-			return status;
-		}
-		for(j=0;j<k-1;j++) {
-			for(i=0;i<m;i++) qkp[i]=Q[i*n+j];
-			H[j*n+k-1]=vect_dotproduct(qk,qkp,m);
-			if ((status=vect_scalarmultsub(H[j*n+k-1],qkp,qk,m,qk))!=HVS_OK) {
-				return status;
-			}
-		}
-		H[k*n+k-1]=M_SQRT(vect_normsq(qk,m));
-		if((status=vect_scalarmult(-1/H[k*n+k-1],qk,m,qkp))!=HVS_OK) {
+	for(j=0;j<n;j++) {
+		H[j*n+n-1]=vect_dotproduct(&Q[j*m],&Q[n*m],m);
+		if ((status=vect_scalarmultsub(H[j*n+n-1],&Q[j*m],&Q[n*m],m,&Q[n*m]))!=HVS_OK) {
 			return status;
 		}
 	}
-	free(qk);
-	free(qkp);
+	H[n*n+n-1]=M_SQRT(vect_normsq(&Q[n*m],m));
+	if (H[n*n+n-1]>=0.0000001) {
+		if((status=vect_scalarmult(-1/H[n*n+n-1],&Q[n*m],m,&Q[n*m]))!=HVS_OK) {
+			return status;
+		}
+	}
+	printf("Q:\n");
+	for(i=0;i<n+1;i++) {
+		for(k=0;k<m;k++)
+		    printf("%Lf\t",Q[i*m+k]);
+		printf("\n");
+	}
+	printf("H:\n");
+	for(i=0;i<n+1;i++) {
+		for(k=0;k<n;k++)
+		    printf("%Lf\t",H[i*n+k]);
+		printf("\n");
+	}
 	return HVS_OK;
 }
